d625f2ddc7a1387033b807bb5c0e90e470ccf2aa
Roxy Knowles <roxypearl11@gmail.com>
2022-03-16 15:24:49 -0700

    commit tree 09a0b48c31b7c6aa5234c51e7d14d109438133c7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const cards = [];
+function writeCards(names,event) {
+  for (let i= 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      debugger;
+  }
+  return cards;
+}
+writeCards(names, event);
+
+function countDown(positiveNumber) {
+while(positiveNumber >= 0) {
+     console.log(positiveNumber);
+    positiveNumber--;
+}
+
+}
